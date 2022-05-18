97d5fd86e331659fcadb3f62ed92366281a79d4c
rhwang10 <robert.t.hwang@gmail.com>
2022-05-17 21:08:06 -0400

    Add tests

---
+++ src/test/java/MockTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MockTest {
+
+    @Test
+    public void someTest() {
+        assertEquals(4, 4);
+    }
+
+    @Test
+    public void anotherTest() {
+        assertEquals("someString", "someString");
+    }
+}
